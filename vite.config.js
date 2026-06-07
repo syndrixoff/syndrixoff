ccb023e528872219e39f09ac6cfcf3a87a38c06a
arxgate <arxgate@gmail.com>
2026-06-07 19:50:31 +0530

    Non-blocking CSS via media="print" + onload swap (eliminates render-blocking request)

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,17 @@ export default {
     cssMinify: "lightningcss",
     outDir: "dist",
   },
+  plugins: [
+    {
+      name: "non-blocking-css",
+      transformIndexHtml(html) {
+        return html.replace(
+          /<link rel="stylesheet" crossorigin href="([^"]+\.css)">/,
+          (_, href) =>
+            `<link rel="stylesheet" media="print" onload="this.media='all'" crossorigin href="${href}">` +
+            `<noscript><link rel="stylesheet" crossorigin href="${href}"></noscript>`
+        );
+      },
+    },
+  ],
 };
